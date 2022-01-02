0af8f25d90b5d45e3b40c0d345ecac8b8c7dbed0
Varun Goel <varun.varungoel.goel@gmail.com>
2022-01-02 14:34:01 +0530

    final commit

--- uapi_mm.h
+++ uapi_mm.h
@@ -1,19 +1,26 @@
-
 #ifndef __UAPI_MM__
 #define __UAPI_MM__
 
 #include <stdint.h>
-void
-mm_init();
 
-void
-mm_instantiate_new_page_family(
-		char *struct_name,
-		uint32_t struct_size);
+void *xcalloc(char *struct_name, int units);
 
-#define MM_REG_STRUCT(struct_name) \
-	(mm_instantiate_new_page_family(#struct_name , sizeof(struct_name)))
+void xfree(void *ptr);
 
+#define XCALLOC(units, struct_name) (xcalloc(#struct_name, units))
 
+#define XFREE(ptr) (xfree(ptr))
 
-#endif   /* __UAPI_MM__ */
+/* Initialization Function */
+void mm_init();
+
+/* Refistration Function */
+void mm_instantiate_new_page_family(char *struct_name, uint32_t struct_size);
+
+#define MM_REG_STRUCT(struct_name)   (mm_instantiate_new_page_family(#struct_name, sizeof(struct_name)))
+
+void mm_print_memory_usage(char *struct_name);
+void mm_print_registered_page_families();
+void mm_print_block_usage();
+
+#endif
